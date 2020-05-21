01b357f0549a69025b6bd3a651a86f28992b2797
helavin <34762647+helavin@users.noreply.github.com>
2020-05-21 22:48:33 +0300

    Create ReadFileInfo application
    Create interface IReadInfo, class ReadInfo, Model class InfoFile.
    Get folders.

---
+++ ConsoleReadFileInfo/Controllers/ReadInfo.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace ConsoleReadFileInfo.Controllers
+{
+    class ReadInfo : IReadInfo
+    {
+        private string path;
+        public string Path
+        {
+            get { return path; }
+            set
+            {
+                if (value != string.Empty)
+                path = value;
+            }
+        }
+
+
+        public void ReadInfoAboutFiles()
+        {
+            Path = GetCurentFolder();
+            if (Path == null)
+            {
+                Console.WriteLine("Папка не выбрана!");
+                return;
+            }
+                
+            Console.WriteLine(Path);
+
+            Queue<string> fileNames = new Queue<string>();
+            Queue<string> directoriesNames = new Queue<string>();
+
+            foreach (var f in Directory.GetFiles(Path))
+            {
+                fileNames.Enqueue(f);
+            }
+
+            foreach (var f in Directory.GetDirectories(Path))
+            {
+                directoriesNames.Enqueue(f);
+            }
+
+            Console.WriteLine("Directories:");
+            foreach (var fn in directoriesNames)
+                Console.WriteLine(fn);
+
+            Console.WriteLine("\nFiles:");
+            foreach (var fn in fileNames)
+                Console.WriteLine(fn);
+
+        }
+
+        internal void Display(ref Queue<string> pathes)
+        {
+            Parallel.ForEach(pathes, (p) =>
+            {
+                Console.WriteLine(p);
+            });
+        }
+
+        internal void GetPathes(string path, ref Queue<string> pathes)
+        {
+            try
+            {
+                var folders = Directory.GetDirectories(path);
+                foreach (var f in folders)
+                {
+                    //Console.WriteLine($"{f} - {Directory.GetAccessControl(f).AreAccessRulesCanonical}");
+                    pathes.Enqueue(f);
+                    GetPathes(f, ref pathes);
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"Нет доступа к папке по пути {path}");                
+            }            
+        }
+
+        internal string GetCurentFolder()
+        {
+            using (System.Windows.Forms.FolderBrowserDialog dialog = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                    return dialog.SelectedPath;
+                else return string.Empty;
+            }
+        }
+
+    }
+}
---
+++ ConsoleReadFileInfo/Program.cs
@@ -0,0 +1,51 @@
+﻿using ConsoleReadFileInfo.Controllers;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace ConsoleReadFileInfo
+{
+    class Program
+    {
+        private static Queue<string> pathes = new Queue<string>();
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            System.Windows.Forms.MessageBox.Show("Укажите путь к папке!");
+            ReadInfo readInfo = new ReadInfo();
+            //readInfo.ReadInfoAboutFiles();
+
+            // Путь к папке
+            string currentFolderPath = readInfo.GetCurentFolder();
+            pathes.Enqueue(currentFolderPath);
+
+            Task taskGetPathes = new Task(() => //{
+            readInfo.GetPathes(currentFolderPath, ref pathes)
+        //    }
+        );
+
+            // задача продолжения
+            Task task2 = taskGetPathes.ContinueWith(task => readInfo.Display(ref pathes));
+
+            taskGetPathes.Start();
+
+            // ждем окончания второй задачи
+            task2.Wait();
+
+            Console.WriteLine("Завершение метода Main");
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
